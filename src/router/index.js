aa4be1d09515750d9666555279b8767c2cc9a8ec
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-09-29 10:33:24 +0200

    feat: Create categories crud in CategoriesView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const router = createRouter({
       path: '/actors',
       name: 'actors',
       component: () => import('../views/ActorsView.vue')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+        component: () => import('../views/CategoriesView.vue')
     }
   ]
 })
