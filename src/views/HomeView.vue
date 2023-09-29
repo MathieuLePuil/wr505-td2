389f708ac15185c24b133b62ff4b0337b1f927f1
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-09-29 09:02:56 +0200

    feat: Show 4 last actors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,16 @@ import { onMounted, ref } from 'vue';
 import axios from "axios";
 
 const data = ref([]);
+const dataActors = ref([]);
 
 onMounted(async () => {
     const response = await axios.get('http://127.0.0.1:8000/api/movies');
     const movies = response.data['hydra:member'];
     data.value = movies.slice(-4).reverse();
+
+    const responseActor = await axios.get('http://127.0.0.1:8000/api/actors');
+    const actors = responseActor.data['hydra:member'];
+    dataActors.value = actors.slice(-4).reverse();
 });
 </script>
 
@@ -16,6 +21,10 @@ onMounted(async () => {
       <div v-for="movie in data" style="border: 1px black solid; padding: 10px; margin-bottom: 10px">
           <h2>{{ movie.title }}</h2>
           <p>{{ movie.description }}</p>
+      </div>
+      <br>
+      <div v-for="actor in dataActors" style="border: 1px black solid; padding: 10px; margin-bottom: 10px;">
+          <h2>{{ actor.firstName }}</h2>
       </div>
   </main>
 </template>
