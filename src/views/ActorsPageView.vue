d57554cf44a513d7b3168eb67b25b8380b9396ba
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-09-29 12:11:59 +0200

    feat: Create actor card in ActorsPageView.vue

---
+++ src/views/ActorsPageView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {useRoute} from "vue-router"
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const route = useRoute()
+const id = route.params.id
+const data = ref([])
+
+onMounted(async () => {
+    const response = await axios.get('http://127.0.0.1:8000/api/actors/' + id)
+    data.value = response.data;
+
+    if (data.value.movies && Array.isArray(data.value.movies)) {
+        const moviePromises = data.value.movies.map(movieURL => axios.get('http://127.0.0.1:8000' + movieURL));
+        const movieResponses = await Promise.all(moviePromises);
+        data.value.movieNames = movieResponses.map(response => response.data.title);
+    }
+})
+</script>
+
+<template>
+    <main>
+        <h1>Acteur n°{{ id }}</h1>
+        <br>
+        <h2>{{ data.firstName }}</h2>
+        <p>{{ data.lastName }}</p>
+        <ul v-for="movie in data.movieNames">
+            <li>{{ movie }}</li>
+        </ul>
+
+
+    </main>
+</template>
