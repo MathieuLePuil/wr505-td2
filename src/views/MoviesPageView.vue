e4df81ea7c6e840506dec94dd77713b59a44f9ab
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-10-17 14:06:01 +0200

    feat: maj

--- src/views/MoviesPageView.vue
+++ src/views/MoviesPageView.vue
@@ -9,33 +9,31 @@ const data = ref([])
 
 onMounted(async () => {
     const response = await axios.get('http://127.0.0.1:8000/api/movies/' + id)
-    data.value = response.data;
+    const movies = response.data;
 
-    if (data.value.category) {
-        const categoryResponse = await axios.get('http://127.0.0.1:8000' + data.value.category);
-        data.value.categoryName = categoryResponse.data.name;
+    for (let movie of movies) {
+        if (movie.category) {
+            const categoryResponse = await axios.get('http://127.0.0.1:8000' + movie.category);
+            movie.categoryName = categoryResponse.data.name;
+        }
+        data.value = movies;
+
     }
-    if (data.value.actor && Array.isArray(data.value.actor)) {
-        const actorPromises = data.value.actor.map(actorURL => axios.get('http://127.0.0.1:8000' + actorURL));
-        const actorResponses = await Promise.all(actorPromises);
-        data.value.actorNames = actorResponses.map(response => response.data.firstName);
-    }
+
 })
 </script>
 
 <template>
     <main>
         <h1>Film n°{{ id }}</h1>
-        <br>
-        <h2>{{ data.title }}</h2>
-        <p>{{ data.description }}</p>
-        <p>{{ data.releaseDate }}</p>
-        <p>{{ data.duration }}</p>
-        <p>{{ data.categoryName }}</p>
-        <ul v-for="actor in data.actorNames">
-            <li>{{ actor }}</li>
-        </ul>
-
+        <div v-for="movie in data">
+            <h2>{{ movie.title }}</h2>
+<!--            <p>{{ data.description }}</p>-->
+<!--            <p>{{ data.releaseDate }}</p>-->
+<!--            <p>{{ data.duration }}</p>-->
+<!--            <p>{{ data.category }}</p>-->
+<!--            <p>{{ data.actor }}</p >-->
+        </div>
 
     </main>
 </template>
