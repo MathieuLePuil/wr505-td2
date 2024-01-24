b0d145811d7a658712378176e2a2ce7a997c4f52
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-01-24 17:12:06 +0100

    feat: add movie

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -6,6 +6,7 @@
             <div class="flex px-10 mt-5">
                 <input v-model="searchText" type="text" placeholder="Search..." class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none">
                 <button @click="searchMovies" class="bg-blue-500 text-white px-4 py-2 rounded">Search</button>
+                <button @click="showAddMovieForm = true" class="bg-blue-500 text-white px-4 py-2 rounded ml-10">Add Movie</button>
             </div>
             <div class="flex px-10 flex-wrap mt-5">
                 <div v-for="movie in movies" :key="movie.id" class="flex flex-col items-center w-1/3 px-3 mb-5">
@@ -31,6 +32,41 @@
                 </div>
             </div>
         </div>
+        <div v-if="showAddMovieForm" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div class="sm:flex sm:items-start">
+                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                    Add Movie
+                                </h3>
+                                <div class="mt-2">
+                                    <form @submit.prevent="addMovie" class="flex flex-col space-y-2">
+                                        <select v-model="newMovie.category_id" required class="border-2">
+                                            <option v-for="category in categories" :value="category.id" :key="category.id">{{ category.name }}</option>
+                                        </select>
+                                        <input v-model="newMovie.title" type="text" placeholder="Title" required class="border-2">
+                                        <textarea v-model="newMovie.description" placeholder="Description" required class="border-2"></textarea>
+                                        <input v-model="newMovie.duration" type="text" placeholder="Duration" required class="border-2">
+                                        <input v-model="newMovie.image" type="text" placeholder="Image URL" required class="border-2">
+                                        <input type="hidden" v-model="newMovie.online">
+                                        <button type="submit">Submit</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <button @click="showAddMovieForm = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -49,10 +85,21 @@ export default {
             currentPage: 1,
             searchText: '',
             isSearching: false,
+            showAddMovieForm: false,
+            newMovie: {
+                category_id: '',
+                title: '',
+                description: '',
+                duration: '',
+                image: '',
+                online: true
+            },
+            categories: []
         };
     },
     created() {
         this.getMovies();
+        this.getCategories();
     },
     methods: {
         toggleDetails(movieId) {
@@ -108,6 +155,46 @@ export default {
                 console.log(error.response.data.code);
             }
         },
+        async getCategories() {
+            const token = localStorage.getItem('user-token');
+            const response = await axios.get('http://localhost:8000/api/categories', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    Accept: 'application/json',
+                },
+            });
+            this.categories = response.data;
+        },
+        async addMovie() {
+            try {
+                const token = localStorage.getItem('user-token');
+                const response = await fetch('http://127.0.0.1:8000/api/movies', {
+                    method: 'POST',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.newMovie)
+                });
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    console.error('Error data:', errorData);
+                } else {
+                    this.showAddMovieForm = false;
+                    this.newMovie = {
+                        category_id: '',
+                        title: '',
+                        description: '',
+                        duration: '',
+                        image: '',
+                        online: true
+                    };
+                }
+            } catch (error) {
+                console.error('Error while adding movie:', error);
+            }
+        }
     },
 };
 </script>
