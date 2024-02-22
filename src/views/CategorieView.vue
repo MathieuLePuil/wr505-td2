9a5ab4d9d6c183de61d14db79d64b13d12bb8c83
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-02-22 15:23:22 +0100

    feat: form to add category

--- src/views/CategorieView.vue
+++ src/views/CategorieView.vue
@@ -5,7 +5,7 @@
             <div class="flex px-10 mt-5">
                 <input v-model="searchText" type="text" placeholder="Search..." class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none">
                 <button @click="searchCategories" class="bg-blue-500 text-white px-4 py-2 rounded">Search</button>
-                <button @click="showAddActorForm = true" class="bg-blue-500 text-white px-4 py-2 rounded ml-10">Add Categorie</button>
+                <button @click="showAddCategoryForm = true" class="bg-blue-500 text-white px-4 py-2 rounded ml-10">Add Categorie</button>
             </div>
             <div class="flex px-10 flex-wrap mt-5">
                 <div v-for="categorie in categories" :key="categorie.id"  class="flex flex-col items-center w-1/3 px-3 mb-5">
@@ -27,32 +27,24 @@
                 </div>
             </div>
         </div>
-        <div v-if="showAddActorForm" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div v-if="showAddCategoryForm" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
             <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
                 <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                     <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                         <div class="sm:flex sm:items-start">
-<!--                            <form @submit.prevent="addActor">-->
-<!--                                <div class="mb-4">-->
-<!--                                    <label for="firstname" class="block text-gray-700 text-sm font-bold mb-2">First Name:</label>-->
-<!--                                    <input type="text" id="firstname" v-model="newActor.firstname" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">-->
-<!--                                </div>-->
-<!--                                <div class="mb-4">-->
-<!--                                    <label for="lastname" class="block text-gray-700 text-sm font-bold mb-2">Last Name:</label>-->
-<!--                                    <input type="text" id="lastname" v-model="newActor.lastname" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">-->
-<!--                                </div>-->
-<!--                                <div class="mb-4">-->
-<!--                                    <label for="nationality" class="block text-gray-700 text-sm font-bold mb-2">Nationality:</label>-->
-<!--                                    <input type="text" id="nationality" v-model="newActor.nationality" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">-->
-<!--                                </div>-->
-<!--                                <div class="flex items-center justify-between">-->
-<!--                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">-->
-<!--                                        Add Categoriries-->
-<!--                                    </button>-->
-<!--                                </div>-->
-<!--                            </form>-->
+                            <form @submit.prevent="addCategory">
+                                <div class="mb-4">
+                                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name:</label>
+                                    <input type="text" id="name" v-model="newCategory.name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                        Add Category
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -72,6 +64,10 @@ export default {
             categories: [],
             searchText: '',
             currentPage: 1,
+            showAddCategoryForm: false,
+            newCategory: {
+                name: ''
+            }
         };
     },
     created() {
@@ -118,6 +114,21 @@ export default {
             if (this.currentPage > 1) {
                 this.currentPage--;
                 this.getCategories();
+            }
+        },
+        async addCategory() {
+            try {
+                const token = localStorage.getItem('user-token');
+                const headers = {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                };
+                const response = await axios.post('http://127.0.0.1:8000/api/categories', this.newCategory, { headers });
+                this.categories.push(response.data);
+                this.showAddCategoryForm = false;
+                this.newCategory = { name: '' };
+            } catch (error) {
+                console.error('Error while adding category:', error);
             }
         },
     },
